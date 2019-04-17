5fdf047a034b87493cecf1875efc182d20f60dfd
unnohideyuki <unno.hideyuki@nifty.com>
2019-04-17 15:51:56 +0900

    abc119d

--- abc119d/abc119d.cs
+++ abc119d/abc119d.cs
@@ -28,6 +28,7 @@ class Program
 {
     public static void Main()
     {
+	long MAX = 1000000000000;
 	int a, b, q;
 
 	var tt = ReadIntArray();
@@ -35,12 +36,16 @@ class Program
 	b = tt[1];
 	q = tt[2];
 
-	var s = new long[a];
-	var t = new long[b];
+	var s = new long[a+2];
+	var t = new long[b+2];
 	var x = new long[q];
+
+	for (int i = 1; i <= a; i++){ s[i] = long.Parse(ReadLine()); }
+	s[0] = -MAX; s[a+1] = MAX;
 	
-	for (int i = 0; i < a; i++){ s[i] = long.Parse(ReadLine()); }
-	for (int i = 0; i < b; i++){ t[i] = long.Parse(ReadLine()); }
+	for (int i = 1; i <= b; i++){ t[i] = long.Parse(ReadLine()); }
+	t[0] = -MAX; t[b+1] = MAX;
+
 	for (int i = 0; i < q; i++){ x[i] = long.Parse(ReadLine()); }
 
 	for (int i = 0; i < q; i++)
@@ -50,57 +55,56 @@ class Program
 	}
     }
 
+    // return i that satisfies a[i] < x <= a[i+1]
+    static int Search(long[] a, long x)
+    {
+	int l = 0, r = a.Length - 2;
+
+	while(true)
+	{
+	    if (l > r){ return -1; } // Must not occur in this program.
+
+	    int m = (l + r) / 2;
+
+	    if (a[m]  >= x)
+	    {
+		r = m - 1;
+	    }
+	    else if (a[m+1] < x)
+	    {
+		l = m + 1;
+	    }
+	    else
+	    {
+		return m;
+	    }
+	}
+    }
+       
     static long Solve(long[] s, long[] t, long x)
     {
 	long ans = 1000000000000;
 
-	long sl = -1, sr = -1;
-	long tl = -1, tr = -1;
+	long sl, sr, tl, tr;
 
-	for (int i = 0; i < s.Length; i++){
-	    if (x > s[i]){ sl = s[i]; }
-
-	    if (x <= s[i])
-	    {
-		sr = s[i];
-		break;
-	    }
-	}
+	int i = Search(s, x); sl = s[i]; sr = s[i+1];
+	int j = Search(t, x); tl = t[j]; tr = t[j+1];
 	
-	for (int i = 0; i < t.Length; i++){
-	    if (x > t[i]){ tl = t[i]; }
+	ans = Math.Max(sr, tr) - x;
 
-	    if (x <= t[i])
-	    {
-		tr = t[i];
-		break;
-	    }
-	}
+	long d = x - Math.Min(sl, tl);
+	ans = Math.Min(ans, d);
 
-	//           X ------->
-	if ((sr >= 0) && (tr >= 0)){
-	    ans = Math.Max(sr, tr) - x;
-	}
+	long d_sr = sr - x;
+	long d_tl = x - tl;
+	long d1 = 2 * Math.Min(d_sr, d_tl) + Math.Max(d_sr, d_tl);
+	ans = Math.Min(ans, d1);
 
-	// <-------- X
-	if ((sl >= 0) && (tl >= 0)){
-	    long d = x - Math.Min(sl, tl);
-	    ans = Math.Min(ans, d);
-	}
+	long d_sl = x - sl;
+	long d_tr = tr - x;
+	long d2 = 2 * Math.Min(d_tr, d_sl) + Math.Max(d_tr, d_sl);
+	ans = Math.Min(ans, d2);
 
-	if ((sr >= 0) && (tl >= 0)){
-	    long d_sr = sr - x;
-	    long d_tl = x - tl;
-	    long d1 = 2 * Math.Min(d_sr, d_tl) + Math.Max(d_sr, d_tl);
-	    ans = Math.Min(ans, d1);
-	}
-
-	if ((tr >= 0) && (sl >= 0)){
-	    long d_sl = x - sl;
-	    long d_tr = tr - x;
-	    long d2 = 2 * Math.Min(d_tr, d_sl) + Math.Max(d_tr, d_sl);
-	    ans = Math.Min(ans, d2);
-	}
 	return ans;
     }
 			    
