851076c0b35bbe5b773b07cc7a8ce455af742685
unnohideyuki <unno.hideyuki@nifty.com>
2019-08-23 19:08:06 +0900

    population-count

--- snippets/MyUtil.cs
+++ snippets/MyUtil.cs
@@ -7,6 +7,16 @@ using static MyUtil;
 
 class MyUtil
 {
+    public static int[] ReadIntArray()
+    {
+	return ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+    }
+
+    public static long[] ReadLongArray()
+    {
+	return ReadLine().Split().Select(x => long.Parse(x)).ToArray();
+    }
+
     public static void AssertEquals<T>(
 				       T expected,
 				       T actual,
@@ -70,13 +80,12 @@ class MyUtil
 	return false;
     }
     
-    public static int[] ReadIntArray()
+    public static int PopulationCount(int x)
     {
-	return ReadLine().Split().Select(x => int.Parse(x)).ToArray();
-    }
-
-    public static long[] ReadLongArray()
-    {
-	return ReadLine().Split().Select(x => long.Parse(x)).ToArray();
+	x = (x & 0x55555555) + (x >> 1 & 0x55555555);
+	x = (x & 0x33333333) + (x >> 2 & 0x33333333);
+	x = (x & 0x0f0f0f0f) + (x >> 4 & 0x0f0f0f0f);
+	x = (x & 0x00ff00ff) + (x >> 8 & 0x00ff00ff);
+	return (x & 0x0000ffff) + (x >>16 & 0x0000ffff);
     }
 }
